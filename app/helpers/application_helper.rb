a3b91e82f04f1154fd4c7decbe02cd8923a6dbe5
Viacheslav Loginov <barthez.slavik@gmail.com>
2021-12-28 05:23:55 +0200

    Fix article

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def color(article)
+    case article
+    when /^Der/ then 'style="color: red;"'
+    when /^Die/ then 'style="color: #004dff"'
+    when /^Das/ then 'style="color: green;"'
+    end
+  end
 end
